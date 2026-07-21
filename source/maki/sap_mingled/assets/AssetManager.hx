3bf67c330ebc47895eb7605fd35df9975e3722ef
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-21 17:36:55 -0500

    dummy intro cutscene asset + intro cutscene (placeholder? might do more with it in the future)

--- source/maki/sap_mingled/assets/AssetManager.hx
+++ source/maki/sap_mingled/assets/AssetManager.hx
@@ -5,4 +5,8 @@ class AssetManager
 	public static function get(path:String, ?ext:String):String return 'assets/$path${ext != null ? '.$ext' : ''}';
 
 	public static function get_path_ost(path:String, ?ext:String = 'ogg'):String return get('ost/$path', ext);
+
+	public static function get_path_game(path:String, ?ext:String = 'png'):String return get('game/$path', ext);
+
+	public static function get_path_game_cutscene(path:String, ?ext:String = 'png'):String return get_path_game('cutscene/$path', ext);
 }
--- source/maki/sap_mingled/states/firsttime/IntroState.hx
+++ source/maki/sap_mingled/states/firsttime/IntroState.hx
@@ -1,6 +1,9 @@
 package maki.sap_mingled.states.firsttime;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxTween;
 
 class IntroState extends FlxState
 {
@@ -8,8 +11,24 @@ class IntroState extends FlxState
 	{
 		super.create();
 
+		var cutsceneAsset = new FlxSprite();
+		cutsceneAsset.loadGraphic(get_path_game_cutscene('intro'));
+		add(cutsceneAsset);
+		cutsceneAsset.screenCenter();
+		cutsceneAsset.alpha = 0;
+		var cutsceneLength = (FlxG.sound.music?.length / 2) ?? 1;
+
 		SAPAudioManager.playMusic({
 			path: get_path_ost('BigBang'),
 		});
+
+		FlxTween.tween(cutsceneAsset, {alpha: 1}, cutsceneLength).then(FlxTween.tween(cutsceneAsset, {alpha: 0}, cutsceneLength, {
+			onComplete: function(t)
+			{
+				cutsceneComplete();
+			}
+		}));
 	}
+
+	function cutsceneComplete() {}
 }
