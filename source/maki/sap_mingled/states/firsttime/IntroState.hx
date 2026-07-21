585dc169044ed604806bab9980d3e4d06feb6659
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-21 17:38:24 -0500

    easing

--- source/maki/sap_mingled/states/firsttime/IntroState.hx
+++ source/maki/sap_mingled/states/firsttime/IntroState.hx
@@ -3,6 +3,7 @@ package maki.sap_mingled.states.firsttime;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 
 class IntroState extends FlxState
@@ -16,13 +17,16 @@ class IntroState extends FlxState
 		add(cutsceneAsset);
 		cutsceneAsset.screenCenter();
 		cutsceneAsset.alpha = 0;
-		var cutsceneLength = (FlxG.sound.music?.length / 2) ?? 1;
+		var cutsceneHalfLength = (FlxG.sound.music?.length / 2) ?? 1;
 
 		SAPAudioManager.playMusic({
 			path: get_path_ost('BigBang'),
 		});
 
-		FlxTween.tween(cutsceneAsset, {alpha: 1}, cutsceneLength).then(FlxTween.tween(cutsceneAsset, {alpha: 0}, cutsceneLength, {
+		FlxTween.tween(cutsceneAsset, {alpha: 1}, cutsceneHalfLength, {
+			ease: FlxEase.sineInOut,
+		}).then(FlxTween.tween(cutsceneAsset, {alpha: 0}, cutsceneHalfLength, {
+			ease: FlxEase.sineInOut,
 			onComplete: function(t)
 			{
 				cutsceneComplete();
