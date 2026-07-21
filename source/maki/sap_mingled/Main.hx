1c50678755de58b10156a143bd3029c0c655384e
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-21 17:52:25 -0500

    hide mouse

--- source/maki/sap_mingled/Main.hx
+++ source/maki/sap_mingled/Main.hx
@@ -1,5 +1,6 @@
 package maki.sap_mingled;
 
+import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.events.Event;
 
@@ -12,8 +13,27 @@ class Main extends FlxGame
 		super(640, 480, StartingState.get(), 60, 60, true, false);
 	}
 
+	public static var mouse_enabled:Bool = false;
+
 	override function create(_:Event)
 	{
 		super.create(_);
+	}
+
+	override function step()
+	{
+		super.step();
+	}
+
+	override function update()
+	{
+		super.update();
+
+		FlxG.mouse.visible = mouse_enabled;
+	}
+
+	override function draw()
+	{
+		super.draw();
 	}
 }
