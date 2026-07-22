b2e7f0b1fdd5d0068290dee6fca4a4e7b323b315
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-22 04:51:32 -0500

    title state buttons

--- source/maki/sap_mingled/states/menus/title/TitleState.hx
+++ source/maki/sap_mingled/states/menus/title/TitleState.hx
@@ -1,7 +1,11 @@
 package maki.sap_mingled.states.menus.title;
 
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxState;
 import flixel.addons.display.FlxBackdrop;
+import flixel.group.FlxSpriteContainer.FlxTypedSpriteContainer;
 import flixel.text.FlxText;
 
 class TitleState extends FlxState
@@ -9,7 +13,10 @@ class TitleState extends FlxState
 	var protoBG:FlxBackdrop;
 	var protoLogo:FlxText;
 
-	var playButton:TitleButton;
+	var buttons:FlxTypedSpriteContainer<TitleButton>;
+	var buttonsCam:FlxCamera;
+	var buttonsCamFollow:FlxObject;
+	var entries:Array<String> = ['Play', 'Options', 'Credits',];
 
 	var selection:Int = 0;
 
@@ -23,23 +30,51 @@ class TitleState extends FlxState
 
 		protoBG.velocity.set(16, 16);
 
-		protoLogo = new FlxText(0, 0, 0, Constants.GAME_TITLE, 16);
+		protoLogo = new FlxText(0, 0, 0, Constants.GAME_TITLE, 32);
 		add(protoLogo);
 		protoLogo.screenCenter();
 
-		protoLogo.setBorderStyle(OUTLINE_CARDINAL, 0xFF000000, 2);
+		protoLogo.setBorderStyle(OUTLINE_CARDINAL, 0xFF000000, 4);
 
-		protoLogo.y -= protoLogo.height * 2;
+		protoLogo.y -= protoLogo.height * 4;
 
-		playButton = new TitleButton('Play', 0);
-		add(playButton);
-		playButton.screenCenter();
+		buttons = new FlxTypedSpriteContainer<TitleButton>();
+		add(buttons);
+
+		var i = 0;
+		for (entry in entries)
+		{
+			if (entry == null) continue;
+			if (entry?.trim()?.length < 1) continue;
+
+			var entry_button = new TitleButton(entry, i);
+			buttons.add(entry_button);
+
+			i++;
+		}
+
+		buttonsCam = new FlxCamera();
+		FlxG.cameras.add(buttonsCam, false);
+		buttonsCam.bgColor = 0x00000000;
+
+		buttons.cameras = [buttonsCam];
+
+		buttonsCamFollow = new FlxObject(FlxG.width / 2);
+		add(buttonsCamFollow);
+
+		buttonsCam.follow(buttonsCamFollow, LOCKON, 0.04);
+		buttonsCam.focusOn(buttonsCamFollow.getPosition());
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		playButton.updateSelection(selection);
+		for (button in buttons)
+		{
+			button.screenCenter(X);
+			button.y = (button.ID * button.size * 2);
+			button.updateSelection(selection);
+		}
 	}
 }
