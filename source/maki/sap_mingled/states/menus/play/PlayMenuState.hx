b4efc9e3a231f28946b4ab72baebdf9f05fe1431
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-22 13:13:29 -0500

    icon cam stuff

--- source/maki/sap_mingled/states/menus/play/PlayMenuState.hx
+++ source/maki/sap_mingled/states/menus/play/PlayMenuState.hx
@@ -1,20 +1,34 @@
 package maki.sap_mingled.states.menus.play;
 
+import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.group.FlxSpriteContainer.FlxTypedSpriteContainer;
+import flixel.util.FlxColor;
 
 @localePrefix('menus.play')
 class PlayMenuState extends FlxState
 {
-	var testIcon:LevelIcon;
+	var icons:FlxTypedSpriteContainer<LevelIcon>;
+	var iconsCam:FlxCamera;
+	var iconsCamFollow:FlxObject;
 
 	override function create()
 	{
 		super.create();
 
-		testIcon = new LevelIcon();
-		add(testIcon);
-		testIcon.screenCenter();
+		icons = new FlxTypedSpriteContainer<LevelIcon>();
+		iconsCam = new FlxCamera();
+		iconsCamFollow = new FlxObject();
+
+		FlxG.cameras.add(iconsCam, false);
+		iconsCam.bgColor = FlxColor.TRANSPARENT;
+
+		add(iconsCamFollow);
+
+		iconsCam.follow(iconsCamFollow, LOCKON, 0.04);
+		iconsCam.focusOn(iconsCamFollow.getPosition());
 	}
 
 	override function update(elapsed:Float)
