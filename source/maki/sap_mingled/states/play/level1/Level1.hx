e6d0ad25f68e142a30d0126b5de17a462cfa5442
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-23 17:08:32 -0500

    level 1 idea

--- source/maki/sap_mingled/states/play/level1/Level1.hx
+++ source/maki/sap_mingled/states/play/level1/Level1.hx
@@ -1,3 +1,35 @@
 package maki.sap_mingled.states.play.level1;
 
-class Level1 extends Level {}
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+
+/**
+ * Port is running,
+ * avoiding bombs and collecting spare ammo when he can.
+ * 
+ * The enemy flies above, spawning bombs.
+ * 
+ * The enemy is only damagable when you have ammo to shoot at them.
+ */
+class Level1 extends Level
+{
+	var bullets:FlxTypedSpriteGroup<FlxSprite>;
+	var obstacles:FlxTypedSpriteGroup<FlxSprite>;
+	var enemy:FlxSprite;
+	var port:FlxSprite;
+
+	override function create()
+	{
+		super.create();
+
+		bullets = new FlxTypedSpriteGroup<FlxSprite>();
+		obstacles = new FlxTypedSpriteGroup<FlxSprite>();
+		enemy = new FlxSprite();
+		port = new FlxSprite();
+
+		add(bullets);
+		add(obstacles);
+		add(enemy);
+		add(port);
+	}
+}
