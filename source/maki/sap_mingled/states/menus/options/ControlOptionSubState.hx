d31bae5d7286e69b2ebec6baa0161f93985989d3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-25 05:51:53 -0500

    rebinding text

--- source/maki/sap_mingled/states/menus/options/ControlOptionSubState.hx
+++ source/maki/sap_mingled/states/menus/options/ControlOptionSubState.hx
@@ -2,10 +2,12 @@ package maki.sap_mingled.states.menus.options;
 
 import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
+@localePrefix('menus.options.controls')
 class ControlOptionSubState extends SAPSubState
 {
 	var control:String;
@@ -17,25 +19,41 @@ class ControlOptionSubState extends SAPSubState
 		this.control = control;
 	}
 
-	var bg = new SAPSprite();
+	var substateCam:FlxCamera;
+	var bg:SAPSprite;
 
-	var substateCam = new FlxCamera();
+	var rebindingText:FlxText;
 
 	override function create()
 	{
 		super.create();
 
+		substateCam = new FlxCamera();
 		FlxG.cameras.add(substateCam, false);
 		substateCam.bgColor = FlxColor.TRANSPARENT;
 
+		bg = new SAPSprite();
 		bg.makeGraphic(Math.round(FlxG.width * 1.1), Math.round(FlxG.height * 1.1), FlxColor.BLACK);
 		bg.alpha = 0;
 		add(bg);
 
-		FlxTween.tween(bg, {alpha: 0.3}, 1, {
+		FlxTween.tween(bg, {alpha: 0.75}, 1, {
 			ease: FlxEase.quintOut,
 		});
 
 		bg.cameras = [substateCam];
+
+		final langKey = '${getClassLocalePrefix(this)}.${control.toLowerCase()}';
+
+		rebindingText = new FlxText(0, 0, 0, 'Rebinding "${getLanguageKey(langKey)}" (Current: ${Reflect.field(Save.data.controls, control)})', 16);
+		add(rebindingText);
+		rebindingText.screenCenter();
+		rebindingText.alpha = 0;
+
+		rebindingText.cameras = [substateCam];
+
+		FlxTween.tween(rebindingText, {alpha: 1}, 1, {
+			ease: FlxEase.quintOut,
+		});
 	}
 }
