421c22b7e9125684f5cba4788ae314319e88eb20
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-25 18:12:11 -0500

    alts will be split just for convienience

--- source/maki/sap_mingled/states/menus/options/ControlsMenuState.hx
+++ source/maki/sap_mingled/states/menus/options/ControlsMenuState.hx
@@ -34,14 +34,13 @@ class ControlsMenuState extends SAPState
 
 		for (control in controls)
 		{
-			if (control.endsWith('_alt')) continue;
+			// if (control.endsWith('_alt')) continue;
 
 			newentry(control, function()
 			{
 				openSubState(new ControlOptionSubState(control));
 			},
-				(entry) -> return OptionsUtil.controls_option_text(this, control, Reflect.field(Save.data.controls, control),
-					Reflect.field(Save.data.controls, '${control}_alt')),);
+				(entry) -> return OptionsUtil.controls_option_text(this, control, Reflect.field(Save.data.controls, control), null),);
 		}
 	}
 
