73bec78c2ee3fd98223dd85f9d6042d412630d9d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-26 23:59:39 -0500

    dont force import format

--- source/maki/sap_mingled/assets/AssetManager.hx
+++ source/maki/sap_mingled/assets/AssetManager.hx
@@ -2,23 +2,8 @@ package maki.sap_mingled.assets;
 
 class AssetManager
 {
-	public static function get(path:String, ?ext:String):String return 'assets/$path${ext != null ? '.$ext' : ''}';
+	public static function get_path(path:String, ?ext:String):String return 'assets/$path${ext != null ? '.$ext' : ''}';
 
-	public static function get_path_ost(path:String, ?ext:String = 'ogg'):String return get('ost/$path', ext);
+	public static function get_path_ost(path:String, ?ext:String = 'ogg'):String return get_path('ost/$path', ext);
 
-	public static function get_path_game(path:String, ?ext:String = 'png'):String return get('game/$path', ext);
-
-	public static function get_path_game_menus(path:String, ?ext:String = 'png'):String return get_path_game('menus/$path', ext);
-
-	public static function get_path_game_menus_play(path:String, ?ext:String = 'png'):String return get_path_game_menus('play/$path', ext);
-
-	public static function get_path_game_sounds(path:String, ?ext:String = 'ogg'):String return get_path_game('sounds/$path', ext);
-
-	public static function get_path_game_cutscene(path:String, ?ext:String = 'png'):String return get_path_game('cutscene/$path', ext);
-
-	public static function get_path_data(path:String, ?ext:String = 'json'):String return get('data/$path', ext);
-
-	public static function get_path_data_lang(path:String, ?ext:String = 'json'):String return get_path_data('lang/$path', ext);
-	
-	public static function get_path_shaders(path:String, ?ext:String = 'frag'):String return get('shaders/$path', ext);
 }
--- source/maki/sap_mingled/import.hx
+++ source/maki/sap_mingled/import.hx
@@ -1,19 +1,29 @@
 #if !macro
 import haxe.rtti.Meta;
+import maki.sap_mingled.save.*;
+//
+// assets
 import maki.sap_mingled.assets.*;
 import maki.sap_mingled.assets.AssetManager.*;
 import maki.sap_mingled.assets.graphics.*;
 import maki.sap_mingled.assets.graphics.ui.*;
+//
+// data
 import maki.sap_mingled.data.*;
 import maki.sap_mingled.data.params.*;
 import maki.sap_mingled.data.params.audiomanager.*;
-import maki.sap_mingled.save.*;
+//
+// states
 import maki.sap_mingled.states.*;
+import maki.sap_mingled.states.debug.*;
+//
+// util (not macro)
 import maki.sap_mingled.util.*;
 import maki.sap_mingled.util.metadata.*;
-import maki.sap_mingled.util.typedefs.null_basic_type.*;
 import maki.sap_mingled.util.typedefs.null_basic_type.flixel.*;
 #end
+//
+// util (macro)
 import maki.sap_mingled.util.macros.*;
 import maki.sap_mingled.util.typedefs.*;
 import maki.sap_mingled.util.typedefs.functions.*;
